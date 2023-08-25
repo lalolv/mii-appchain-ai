b7b56a439874bf2c3d61ad3927d3f46ba6d3b8c1
Lalo <work@lalo.im>
2023-08-25 09:47:19 +0800

    加载默认图片

--- src/components/MyAvatar.vue
+++ src/components/MyAvatar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
 const props = defineProps({
   qnkey: String
@@ -7,21 +7,27 @@ const props = defineProps({
 const quUrl = ref('')
 
 // 初始化
-onMounted(()=>{
+onMounted(() => {
   quUrl.value = import.meta.env.VITE_QN_URL
 })
 
+function useDefaultImage(event) {
+  event.target.src = '@/assets/thumbnail.png'
+}
 </script>
 
 <template>
-  <a class="h-auto w-auto rounded-2xl overflow-hidden cursor-pointer hover:drop-shadow-2xl" 
-    href="https://opensea.io/appchain_ai" target="_blank">
+  <a
+    class="h-auto w-auto rounded-2xl overflow-hidden cursor-pointer hover:drop-shadow-2xl"
+    href="https://opensea.io/appchain_ai"
+    target="_blank"
+  >
     <img
-      class="w-full h-full object-cover transition ease-in-out 
-        hover:scale-125 transform-gpu"
+      class="w-full h-full object-cover transition ease-in-out hover:scale-125 transform-gpu"
       :src="`${quUrl}/${qnkey}-avatar_thumb`"
       alt="Modern building architecture"
       loading="lazy"
+      @error.once="useDefaultImage"
     />
   </a>
 </template>
